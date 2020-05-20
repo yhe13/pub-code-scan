03e11208c1c45427d0ec558a26ca3348a948e6da
Yong He <60520065+yhe13@users.noreply.github.com>
2020-05-20 14:56:53 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,7 +3,10 @@ int g_vars;
 
 class SomeClass {
 public:
-    SomeClass() {}
+    SomeClass() {
+        pub = 0;
+        g_vars = 123456;
+    }
     int pub;
     int get_g_var(){
         return g_vars;
